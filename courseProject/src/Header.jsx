44233ee4d4a5496a2e56a11b483518f01a7c888d
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-15 22:54:12 +0300

    first commit

---
+++ courseProject/src/Header.jsx
@@ -0,0 +1,12 @@
+
+import './css/Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='title'>UDEMY KURSLARIM</div>
+    </div>
+  )
+}
+
+export default Header
