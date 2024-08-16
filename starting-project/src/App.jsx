81aead44ce026f26282cc09c5dc01b14c81ffd4f
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-16 14:31:33 +0300

    props

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,26 +1,41 @@
-
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are going to build!
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
 }
 
-
-
-
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src=" " alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
+
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title=" Components"
+              description="The core UI building blocks"
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
