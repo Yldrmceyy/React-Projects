747481b7689f9dd3f2a496f45e1a955fd5970758
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-16 16:40:28 +0300

    imported CoreConcept

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -5,17 +5,9 @@
 import { CORE_CONCEPTS } from "./data.js";
 
 import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
