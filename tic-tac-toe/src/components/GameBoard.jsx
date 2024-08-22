38619d67b6d8626b06b89375748ff6e60dfb9350
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-22 16:56:57 +0300

    Track and update player moves using turns state

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -6,18 +6,24 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol }) {
+export default function GameBoard({onSelectSquare,turns}) {
+let gameBoard=initialGameBoard;
 
-  const[gameBoard, setGameBoard]= useState(initialGameBoard);
+for (const turn of turns){
+  const{square,player}=turn;
+  const {row,col}=square;
+  gameBoard[row][col]=player;
+}
+  // const[gameBoard, setGameBoard]= useState(initialGameBoard);
   
-    function handleSelectSquare(rowIndex,colIndex){
-      setGameBoard((prevGameBoard) => {
-        const updateBoard=[...prevGameBoard.map(innerArray=> [...innerArray])];
-        updateBoard[rowIndex][colIndex]= activePlayerSymbol;
-        return updateBoard;
-      })
-      onSelectSquare();
-    }
+  //   function handleSelectSquare(rowIndex,colIndex){
+  //     setGameBoard((prevGameBoard) => {
+  //       const updateBoard=[...prevGameBoard.map(innerArray=> [...innerArray])];
+  //       updateBoard[rowIndex][colIndex]= activePlayerSymbol;
+  //       return updateBoard;
+  //     })
+  //     onSelectSquare();
+  //   }
 
   return (
     <ol id="game-board">
@@ -26,7 +32,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=>handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button onClick={()=>onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
