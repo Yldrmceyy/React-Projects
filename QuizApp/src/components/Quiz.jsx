930bbfdd5beb21dec021717971d41427252102fb
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-27 23:25:35 +0300

    added

--- QuizApp/src/components/Quiz.jsx
+++ QuizApp/src/components/Quiz.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 
 export default function Quiz() {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+
+  const [userAnswers, setUserAnswers]=useState([])
   return <p>Currenty active Question</p>;
 }
