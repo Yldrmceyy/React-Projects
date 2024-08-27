3eee530df762621591f79ee53318dcd8cd32e715
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-27 23:45:07 +0300

    handleSelectAnswer function is created

--- QuizApp/src/components/Quiz.jsx
+++ QuizApp/src/components/Quiz.jsx
@@ -5,9 +5,27 @@ export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
+function handleSelectAnswer(selectedAnswer){
+setUserAnswers();
+}
+
+
+
   return (
     <div id="question">
-      <h2> {QUESTIONS[activeQuestionIndex]}</h2>;
+      <h2> {QUESTIONS[activeQuestionIndex]}</h2>
+      <ul id="question">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer)=>(
+            <li key={answer} className="answer">
+            <button onClick={()=>handleSelectAnswer(answer)}>
+                {answer}
+            </button>
+
+            </li>
+        ))}
+      </ul>
+
+
     </div>
   );
 }
