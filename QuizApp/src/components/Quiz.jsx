00b976fbf78bcec5281328376475fb6d13ca9625
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-28 01:26:43 +0300

    updated

--- QuizApp/src/components/Quiz.jsx
+++ QuizApp/src/components/Quiz.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import QUESTIONS from "../question";
+import question from "../question";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
 
-
-
+  const quizIsComplete = activeQuestionIndex === question.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
@@ -17,6 +16,22 @@ export default function Quiz() {
   if (activeQuestionIndex >= QUESTIONS.length) {
     return <div>Quiz completed!</div>;
   }
+
+  if (quizIsComplete) {
+    return( <div id="summary">
+    <img src={quizCompleteImg} alt="Trophy icon"></img>
+        <h2>Quiz completed!</h2>
+    </div>
+    )
+  }
+
+
+  
+
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
 
   return (
     <div id="question">
