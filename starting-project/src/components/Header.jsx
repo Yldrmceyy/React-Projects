73cafab81ca8a3433a6ad2fae06c4902deca1d2e
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-10 16:38:07 +0300

    updated

--- starting-project/src/components/Header.jsx
+++ starting-project/src/components/Header.jsx
@@ -1,10 +1,5 @@
-
-
-
-import reactImg from '../assets/react-core-concepts.png';
-import './Header.css'
-
-
+import reactImg from "../assets/react-core-concepts.png";
+import "./Header.css";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -12,16 +7,15 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 export default function Header() {
-    const description = reactDescriptions[getRandomInt(2)];
-    return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-    );
-  }
-  
+  const description = reactDescriptions[getRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
