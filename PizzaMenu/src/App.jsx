aeb299de2654745e3a6312566ab947b5b3012fc6
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-10 23:09:33 +0300

    numpizzas length condition added

--- PizzaMenu/src/App.jsx
+++ PizzaMenu/src/App.jsx
@@ -16,16 +16,22 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <p>
-        Authentic Italian cuisine. 6 creative dishes to choose from. All from
-        our stone oven, all organic, all delicious.
-      </p>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
     </main>
   );
 }
@@ -37,7 +43,7 @@ function Pizza({ pizzaObj }) {
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.soldOut ? "SOLD-OUT" : "$"+pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD-OUT" : "$" + pizzaObj.price}</span>
       </div>
     </li>
   );
