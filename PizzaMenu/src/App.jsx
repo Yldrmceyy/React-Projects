7bcd6482afc1cf24b49e44c4c07d262805947153
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-10 23:00:53 +0300

    updated

--- PizzaMenu/src/App.jsx
+++ PizzaMenu/src/App.jsx
@@ -1,15 +1,57 @@
+import pizzaData from "./pizzaData";
+import "./App.css";
 
-
-import './App.css'
-
-function App() {
-
-
+function Header() {
   return (
-    <>
-     
-    </>
-  )
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
-export default App
+function Menu() {
+  const numPizzas = pizzaData.length;
+
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function Pizza({ pizzaObj }) {
+  return (
+    <li className="pizza">
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <p>Price: ${pizzaObj.price}</p>
+      </div>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <Menu />
+      </div>
+    </>
+  );
+}
+
+export default App;
