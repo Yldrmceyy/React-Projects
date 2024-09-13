8e4c00922b7dc2fc8ef1410a74ec2e1ba6e45071
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-13 18:18:24 +0300

    completed

--- FlashCards/src/App.jsx
+++ FlashCards/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index.css";
 
 const questions = [
@@ -35,11 +36,23 @@ const questions = [
 ];
 
 function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+function handleClick(id) {
+  setSelectedId(id !== selectedId ? id: null);
+  }
+
   return (
     <div className="flashcards">
       {questions.map((question) => (
-        <div key={question.id}>
-          <p >{question.question} </p>
+        <div
+          key={question.id}
+          onClick={()=>handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}{" "}
+          </p>
         </div>
       ))}
     </div>
