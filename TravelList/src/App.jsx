ccd92a4f98e683c267b1d5f346cf333bdeb3f0b7
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-13 14:30:38 +0300

    updated

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -1,7 +1,6 @@
 import "./index.css";
 import { useState } from "react";
 
-
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -14,15 +13,23 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] =useState(5);
+  
+  
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
-  }
+
+    if(!description) return;
+    const newItem={description,quantity,packed:false, id:Date.now()};
+    console.log(newItem);
+  };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e)=>{
+        setQuantity(Number(e.target.value))
+      }}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
