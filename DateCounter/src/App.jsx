9bc61a0259cfb3430a8807c9e05d184555c9b081
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-13 18:50:17 +0300

    e.target.value changed as number

--- DateCounter/src/App.jsx
+++ DateCounter/src/App.jsx
@@ -10,12 +10,15 @@ function Counter() {
   return (
     <div>
       <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
 
-<input type="range"  min="0" max="10"/>
-
-        <button onClick={() => setStep((s) => s - 1)}> - </button>
         <span> Step : {step} </span>
-        <button onClick={() => setStep((s) => s + 1)}> + </button>
       </div>
 
       <div>
