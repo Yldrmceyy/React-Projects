8133a298d176e39e3fe373461609e107bf894393
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-10-12 22:47:50 +0300

    Added Clear List button to remove all items from the packing list

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -26,6 +26,9 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    setItems([]);
+  }
   return (
     <div className="app">
       <Logo />
@@ -34,6 +37,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -90,18 +94,22 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem ,onClearList}) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
 
-  if(sortBy=== "input") sortedItems =items;
+  if (sortBy === "input") sortedItems = items;
 
-  if(sortBy==="description") sortedItems=items.slice().sort((a,b)=>a.description.localeCompare(b.description));
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
 
-  if(sortBy==="packed") 
-    sortedItems=items.slice().sort((a,b)=>Number(a.packed)- Number(b.packed));
-
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
 
   return (
     <div className="list">
@@ -121,7 +129,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
-        <button>Clear List</button>
+        <button onClick={onClearList}>Clear List</button>
       </div>
     </div>
   );
