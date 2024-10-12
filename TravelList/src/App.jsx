506836c4502f54a222442f5ead82eb6142ab713c
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-10-12 22:31:13 +0300

    V

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -99,7 +99,8 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
 
   if(sortBy==="description") sortedItems=items.slice().sort((a,b)=>a.description.localeCompare(b.description));
 
-   
+  if(sortBy==="packed") 
+    sortedItems=items.slice().sort((a,b)=>Number(a.packed)- Number(b.packed));
 
 
   return (
