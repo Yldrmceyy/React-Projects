fddf3042c1f117c29be66fa6c264d5b7e32483f7
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-10-15 15:06:48 +0300

    Add conditional rendering for Output and Reset components with fragment usage

--- TipCalculator/src/App.jsx
+++ TipCalculator/src/App.jsx
@@ -14,13 +14,13 @@ function TipCalculator() {
   const [percentage1, setPercentage1] = useState(0);
   const [percentage2, setPercentage2] = useState(0);
 
-  const tip = bill * ((percentage1 + percentage2)/2/100);
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
 
-function handleReset() { 
-  setBill("");
-  setPercentage1(0);
-  setPercentage2(0);
- }
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
 
   return (
     <div>
@@ -31,8 +31,12 @@ function handleReset() {
       <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
         How did your friend like the service?
       </SelectPercentage>
-      <Output bill={bill} tip={tip}/>
-      <Reset onReset={handleReset}/>
+      {bill > 0 && (
+        <>
+          <Output bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
@@ -68,15 +72,18 @@ function SelectPercentage({ children, percentage, onSelect }) {
   );
 }
 
-function Output({ bill,tip }) {
+function Output({ bill, tip }) {
   return (
     <div>
-      <h3> You pay {bill+tip} (${bill} + ${tip} tip)</h3>
+      <h3>
+        {" "}
+        You pay {bill + tip} (${bill} + ${tip} tip)
+      </h3>
     </div>
   );
 }
 
-function Reset({onReset}) {
+function Reset({ onReset }) {
   return (
     <div>
       <button onClick={onReset}>Reset</button>
