0eada1417816531b5d9833dd3be4fc5d54bd7d08
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-10-15 17:22:07 +0300

    Added friends state and passed to FriendList

--- Eat-N-Split/src/App.jsx
+++ Eat-N-Split/src/App.jsx
@@ -30,6 +30,7 @@ function Button({ children, onClick }) {
 }
 
 export default function App() {
+  const [friends,setFriends]=useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   function handleShowAddFriend() {
@@ -39,7 +40,7 @@ export default function App() {
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendList />
+        <FriendList friends={friends} />
         {showAddFriend && <FormAddFriend />}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Close" : "Add friend"}
@@ -50,8 +51,8 @@ export default function App() {
   );
 }
 
-function FriendList() {
-  const friends = initialFriends;
+function FriendList({friends}) {
+ 
   return (
     <ul>
       {friends.map((friend) => (
