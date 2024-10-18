ab52c15a5d323e862f172006084b59243cd4e7a7
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-10-18 13:29:02 +0300

    listed bungalow items

--- BungalowRent/src/App.jsx
+++ BungalowRent/src/App.jsx
@@ -1,59 +1,61 @@
-const bungalows = [
+const initialBungalows = [
   {
     id: 1,
     name: "Bungalow A",
-    image: "https://via.placeholder.com/100",
+    image: "https://loremflickr.com/320/320/house",
     price: 200,
   },
   {
     id: 2,
     name: "Bungalow B",
-    image: "https://via.placeholder.com/100",
+    image: "https://loremflickr.com/320/320/house",
     price: 250,
   },
   {
     id: 3,
     name: "Bungalow C",
-    image: "https://via.placeholder.com/100",
+    image: "https://loremflickr.com/320/320/house",
     price: 300,
   },
 ];
 
 function App() {
   return (
-    <>
-      <BungalowList />
-      <ul className="bungalow-list">
-        <BungalowItem name="Bungalow A" price={200} />
-        <BungalowItem name="Bungalow B" price={250} />
-        <BungalowItem name="Bungalow C" price={300} />
-      </ul>
-      <FormAddBungalow />
-      <FormReservation />
-    </>
+    <div className="app">
+      <div className="left-column">
+        <BungalowList />
+        <FormAddBungalow />
+      </div>
+      <div className="right-column">
+        <FormReservation />
+      </div>
+    </div>
   );
 }
 
 export default App;
 
 function BungalowList() {
+  const bungalow = initialBungalows;
   return (
-    <div className="app">
-      <div className="sidebar">
-        <h1>Bungalow List</h1>
-        <ul className="bungalow-list"></ul>
-      </div>
+    <div className="bungalow-list">
+      <h1>Bungalow List</h1>
+      <ul>
+        {bungalow.map((bungalow) => (
+          <BungalowItem bungalow={bungalow} key={bungalow.id} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-function BungalowItem({ name, price }) {
+function BungalowItem({ bungalow }) {
   return (
     <li className="bungalow-item">
-      <img src="https://via.placeholder.com/100" alt="Bungalow Image" />
-      <h3>{name}</h3>
-      <p>Price: ${price}</p>
-      <button className="button">Select</button>
+      <img src={bungalow.image} alt="Bungalow Image" />
+      <h3 >{bungalow.name}</h3>
+      <p>Price: ${bungalow.price}</p>
+      <button className="button">Select For Reservation</button>
     </li>
   );
 }
