b1300703ce5d76f5144dae78ba16e234aa9ded84
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-11-23 15:32:12 +0300

    Add fetch logic to load quiz questions from mock API

--- ReactQuiz/src/App.jsx
+++ ReactQuiz/src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import Header from "./Header.jsx";
 import Top from "./Top.jsx";
+import {useEffect} from  "react";
 
 function App() {
+
+  useEffect(function(){
+    fetch("http://localhost:9000/questions")
+    .then((res)=>res.json())
+    .then((data)=>console.log(data))
+    .catch((err) => console.log(`Error: ${err.message}`));
+  },[]);
+
   return (
     <div className="app">
       <Header />
