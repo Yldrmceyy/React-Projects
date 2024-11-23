68f6d802cd048d00e99d659e807f6ea53789983f
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-11-23 18:01:06 +0300

    p tag changed as h3

--- ReactQuiz/src/StartScreen.jsx
+++ ReactQuiz/src/StartScreen.jsx
@@ -3,8 +3,8 @@ export default  function StartScreen({numQuestions}) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
-      <p>{numQuestions} question to tet your React mastery</p>
-      <button className="btn">Lets Start!</button>
+      <h3>{numQuestions} question to tet your React mastery</h3>
+      <button className="btn btn-ui">Lets Start!</button>
     </div>
   );
 }
