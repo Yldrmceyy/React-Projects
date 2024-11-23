a0b2c8ed7f92729d308c52b74996919da3fc3d6d
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-11-23 18:37:20 +0300

    Add index state and create Question component to display questions and options

--- ReactQuiz/src/Question.jsx
+++ ReactQuiz/src/Question.jsx
@@ -1,8 +1,13 @@
-
-function Question() {
+function Question({ question }) {
+  console.log(question);
   return (
-    <div>Question</div>
-  )
+    <div>
+      <h4>{question.question}</h4>
+      <div className="options">
+      {question.options.map((option)=><button className="btn btn-option" key={option}>{option}</button>)}
+      </div>
+    </div>
+  );
 }
 
-export default Question
+export default Question;
