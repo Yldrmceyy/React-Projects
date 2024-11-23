75681d3dc0a9e7d00afaf5daa29ba9f80a939acd
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-11-23 15:13:49 +0300

    updated

--- ReactQuiz/src/App.jsx
+++ ReactQuiz/src/App.jsx
@@ -1,18 +1,14 @@
 import "./App.css";
 import Header from "./Header.jsx";
-import DateCounter from "./DateCounter.jsx";
 import Top from "./Top.jsx";
+
 function App() {
   return (
     <div className="app">
       <Header />
       <Top>
-        <p>
-          1/15
-        </p>
-        <p>
-          Questions?
-        </p>
+        <p>1/15</p>
+        <p>Questions?</p>
       </Top>
     </div>
   );
