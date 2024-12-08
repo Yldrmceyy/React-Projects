cde3a033fc8ef8a79808b95fdaec4f1279209cb8
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-12-08 16:49:45 +0300

    updated index as index+1

--- ReactQuiz/src/components/Progress.jsx
+++ ReactQuiz/src/components/Progress.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Progress({index,numQuestion}) {
   return (
     <header className='progress'>
-        <p>Question <strong>{index}</strong> / {numQuestion} </p>
+        <p>Question <strong>{index+1}</strong> / {numQuestion} </p>
     </header>
   )
 }
