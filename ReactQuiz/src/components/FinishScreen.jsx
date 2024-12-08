23ab232fb4b2c04179c3f9a8dcacb206fc90408d
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-12-08 21:57:53 +0300

    Added emojis for quiz results

--- ReactQuiz/src/components/FinishScreen.jsx
+++ ReactQuiz/src/components/FinishScreen.jsx
@@ -2,12 +2,24 @@ import React from "react";
 
 function FinishScreen({ points, maxPossiblePoints }) {
   const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 0 && percentage < 50) emoji = "🤔";
+  if (percentage === 0) emoji = "🤦🏽‍♀️";
+
   return (
-    <p className="result">
-      {" "}
-      You scored <strong>{points}</strong> out of {maxPossiblePoints} (
-      {Math.ceil(percentage)} %)
-    </p>
+    <>
+      <p className="result">
+        {" "}
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)} %)
+      </p>
+
+      <p className="highscore"> (Highcore: X points)</p>
+    </>
   );
 }
 
