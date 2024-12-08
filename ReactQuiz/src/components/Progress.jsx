9ba9b4d4072d21cbbf97ff9dbe1bf69332daa9f0
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-12-08 20:35:07 +0300

    Added Progress component to track quiz progress, questions, and points with a dynamic progress bar.

--- ReactQuiz/src/components/Progress.jsx
+++ ReactQuiz/src/components/Progress.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function Progress({index,numQuestion}) {
+function Progress({ index, numQuestions, points, maxPossiblePoints,answer }) {
   return (
-    <header className='progress'>
-        <p>Question <strong>{index+1}</strong> / {numQuestion} </p>
+    <header className="progress">
+    <progress max={numQuestions} value={index+ Number(answer !== null )}/>
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestions}{" "}
+      </p>
+
+
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+
+      </p>
     </header>
-  )
+  );
 }
 
-export default Progress
+export default Progress;
